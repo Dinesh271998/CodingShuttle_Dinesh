ab13297170a9091bea748342880d6e66bcd28eb5
Dinesh singh tomar <dineshsinght517@gmail.com>
2025-03-16 19:16:37 +0530

    this is my first commit.

---
+++ Practice/Practice/src/main/java/com/CodingShuttle/Practice/PracticeApplication.java
@@ -0,0 +1,15 @@
+package com.CodingShuttle.Practice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
