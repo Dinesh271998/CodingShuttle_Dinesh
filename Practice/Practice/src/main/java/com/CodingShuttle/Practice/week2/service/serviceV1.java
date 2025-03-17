7cd485c79cba5c3d4c9ba0548b350229df737442
Dinesh singh tomar <dineshsinght517@gmail.com>
2025-03-17 15:45:48 +0530

    This is my first service layer commit

---
+++ Practice/Practice/src/main/java/com/CodingShuttle/Practice/week2/service/serviceV1.java
@@ -0,0 +1,7 @@
+package com.CodingShuttle.Practice.week2.service;
+
+public class serviceV1 {
+    public static void main(String[] args) {
+        System.out.println("new service is created for personal things");
+    }
+}
