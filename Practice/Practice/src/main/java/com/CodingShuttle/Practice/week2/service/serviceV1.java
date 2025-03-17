ab2d8aca7b8832f5b983cbdcb38c26ac3d4b6af3
Dinesh singh tomar <dineshsinght517@gmail.com>
2025-03-17 16:38:43 +0530

    New commit

--- Practice/Practice/src/main/java/com/CodingShuttle/Practice/week2/service/serviceV1.java
+++ Practice/Practice/src/main/java/com/CodingShuttle/Practice/week2/service/serviceV1.java
@@ -3,5 +3,6 @@ package com.CodingShuttle.Practice.week2.service;
 public class serviceV1 {
     public static void main(String[] args) {
         System.out.println("new service is created for personal things");
+        System.out.println("Trying to check new commit update");
     }
 }
